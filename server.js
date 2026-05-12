1edfd47bda08ae1267b7fb4d477bbf0e9f547af2
Niranjane <niranjancheetah98@gmail.com>
2026-05-12 16:35:30 +0530

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+// Import Express
+const express = require('express');
+const app = express();
+
+// Define a port
+const PORT = 8000;
+
+// Middleware (optional)
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello from Express server 🚀');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
