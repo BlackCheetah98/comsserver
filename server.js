8160cedfe404b8d68fdc7be9cdc4902ff1568ccb
Niranjane <niranjancheetah98@gmail.com>
2026-05-12 18:35:11 +0530

    init commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Hello from Express server 🚀');
+  res.send('Hello from Express servesfsfr 🚀');
 });
 
 // Start server
